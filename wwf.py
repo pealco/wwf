4b56ba7d2bc2577a2337b694e4d7cdc64c53d823
Pedro Alcocer <pealco@gmail.com>
2011-02-15 03:34:45 -0500

    Added tests.

--- wwf.py
+++ wwf.py
@@ -2,46 +2,40 @@ import sys
 import operator
 import string
 import re
-from itertools import permutations, chain
-        
-class memoized(object):
-   def __init__(self, func):
-      self.func = func
-      self.cache = {}
-   def __call__(self, *args):
-      try:
-         return self.cache[args]
-      except KeyError:
-         value = self.func(*args)
-         self.cache[args] = value
-         return value
-      except TypeError:
-         return self.func(*args)
-   def __repr__(self):
-      return self.func.__doc__
-   def __get__(self, obj, objtype):
-      return functools.partial(self.__call__, obj)
+import itertools
+
+WORD_LIST_FILE = "enable1.txt"
+ALPHABET = string.ascii_uppercase
+LETTER_VALUES = {
+    "a": 1,  "b": 4, "c": 4, "d": 2, "e": 1, "f": 4, "g": 3, "h": 3, "i": 1, 
+    "j": 10, "k": 5, "l": 2, "m": 4, "n": 2, "o": 1, "p": 4, "q": 10, 
+    "r": 1,  "s": 1, "t": 1, "u": 2, "v": 5, "w": 4, "x": 8, "y": 3, "z": 10, "*": 0
+}
 
 def flatten(listOfLists):
-    return chain.from_iterable(listOfLists)
+    return itertools.chain.from_iterable(listOfLists)
+    
+def permutations(letters, r=None):
+    if r == None: r = len(letters)
+    if len(letters) == 1:
+        raise ValueError("Can't find permutations of a single letter.")
+    if r < 2:
+        raise ValueError("r is too small; it must at least 2.")
+    if r > len(letters):
+        raise ValueError("r is too big; it can only be as big as len(letters)")
+    
+    return itertools.permutations(letters, r)
 
-@memoized
 def point_value(word):
-    word = word.translate(None, ALPHABET)
-    return sum(LETTER_VALUES[letter] for letter in word)
-
-@memoized
-def my_permutations(letters, r):
-    return permutations(letters, r)
+    return sum(LETTER_VALUES[letter] for letter in word if letter not in ALPHABET)
     
 def create_word_list(f):
-    word_file = open(f, "r")
-    word_list = set(word.rstrip() for word in word_file)
-    word_file.close()
-    
+    with open(f, "r") as word_file:
+        word_list = set(tuple(word.rstrip()) for word in word_file)
+            
     return word_list
 
-def compute_possible_racks(rack_string):
+def compute_racks(rack_string):
     racks = [rack_string]
     for rack in racks:
         for index, tile in enumerate(rack):
@@ -49,47 +43,33 @@ def compute_possible_racks(rack_string):
                 for letter in ALPHABET:
                     racks += [rack[:index] + letter + rack[index+1:]]
     
-    racks = set(rack for rack in racks if '*' not in rack)
-    
-    return racks
+    return set(rack for rack in racks if '*' not in rack)
 
 def exists(word):
-    return word.lower() in word_list
+    return word in word_list
+
+def lowercase(iterable):
+    return tuple(s.lower() for s in iterable)
+    
+def get_all_perms(rack):
+    return flatten(permutations(rack, span) for span in xrange(2, len(rack) + 1))
 
 def get_perms(racks):
-    return set(flatten(my_permutations(rack, span) for rack in racks for span in xrange(2, len(rack) + 1)))
+    return flatten(get_all_perms(rack) for rack in racks)
 
 def get_candidates(perms):
-    candidates = {}
-    for perm in perms:
-        this_perm = ''.join(perm)
-        if exists(this_perm):
-            candidates[this_perm] = point_value(this_perm)
-    return candidates
+    return dict((''.join(perm), point_value(perm)) for perm in perms if exists(lowercase(perm)))
 
 def compute_candidates(racks):
     perms = get_perms(racks)
-    candidates = get_candidates(perms)
-    
-    return candidates    
-
+    return get_candidates(perms)
     
 def sort_candidates(candidates):
     candidates = dict([(word, value) for (word, value) in candidates.items() if re.match(constraints, word)])
-    sorted_candidates = sorted([(v, k) for (k, v) in candidates.items()], reverse=True)
-    
-    return sorted_candidates
+    return sorted([(v, k) for (k, v) in candidates.items()], reverse=True)
 
 if __name__ == '__main__':
-    
-    WORD_LIST_FILE = "enable1.txt"
-    ALPHABET = string.ascii_uppercase
-    LETTER_VALUES = {
-        "a": 1,  "b": 4, "c": 4, "d": 2, "e": 1, "f": 4, "g": 3, "h": 3, "i": 1, 
-        "j": 10, "k": 5, "l": 2, "m": 4, "n": 2, "o": 1, "p": 4, "q": 10, 
-        "r": 1,  "s": 1, "t": 1, "u": 2, "v": 5, "w": 4, "x": 8, "y": 3, "z": 10, "*": 0
-    }
-    
+
     try:
         rack_string = sys.argv[1]
     except IndexError:
@@ -105,17 +85,16 @@ if __name__ == '__main__':
     rack_string += constraints.translate(None, string.punctuation)  
     
     if rack_string.count('*') > 2:
-        raise ValueError("You can only have a maximum of two blank rack_strings.")
+        raise ValueError("You can only have a maximum of two blank tiles.")
     
-    possible_racks = compute_possible_racks(rack_string)
-    candidates = compute_candidates(possible_racks)
+    racks = compute_racks(rack_string)
+    candidates = compute_candidates(racks)
     sorted_candidates = sort_candidates(candidates)
     
     if sorted_candidates:
         print "\nPoints\tWord\tLength"    
         for (points, word) in sorted_candidates:
-            pass
-            #print points, "\t", word, "\t", len(word)
+            print points, "\t", word, "\t", len(word)
         print len(sorted_candidates), "words"
     else:
         print "\nNo words found."
--- wwftest.py
+++ wwftest.py
@@ -1,2 +1,96 @@
 import wwf
 import unittest
+
+class KnownValues(unittest.TestCase):
+    pointKnownValues = \
+        (('abc',   9),
+         ('boom',  10),
+         ('great', 7),
+         ('qi',    11),
+         ('Qi',    1),
+         ('QIs',   1),
+         ('IS',    0))
+                        
+    permutationsKnownValues = \
+        (('abc', len('abc'),
+            [('a', 'b', 'c'), 
+             ('a', 'c', 'b'), 
+             ('b', 'a', 'c'), 
+             ('b', 'c', 'a'), 
+             ('c', 'a', 'b'), 
+             ('c', 'b', 'a')]),
+         ('wq', len('wq'),
+            [('w', 'q'), 
+             ('q', 'w')]),
+         ('zz*', len('zz*'),
+            [('z', 'z', '*'), 
+             ('z', '*', 'z'), 
+             ('z', 'z', '*'), 
+             ('z', '*', 'z'), 
+             ('*', 'z', 'z'), 
+             ('*', 'z', 'z')]),
+         ('asdf', len('asdf'),
+            [('a', 's', 'd', 'f'),
+             ('a', 's', 'f', 'd'),
+             ('a', 'd', 's', 'f'),
+             ('a', 'd', 'f', 's'),
+             ('a', 'f', 's', 'd'),
+             ('a', 'f', 'd', 's'),
+             ('s', 'a', 'd', 'f'),
+             ('s', 'a', 'f', 'd'),
+             ('s', 'd', 'a', 'f'),
+             ('s', 'd', 'f', 'a'),
+             ('s', 'f', 'a', 'd'),
+             ('s', 'f', 'd', 'a'),
+             ('d', 'a', 's', 'f'),
+             ('d', 'a', 'f', 's'),
+             ('d', 's', 'a', 'f'),
+             ('d', 's', 'f', 'a'),
+             ('d', 'f', 'a', 's'),
+             ('d', 'f', 's', 'a'),
+             ('f', 'a', 's', 'd'),
+             ('f', 'a', 'd', 's'),
+             ('f', 's', 'a', 'd'),
+             ('f', 's', 'd', 'a'),
+             ('f', 'd', 'a', 's'),
+             ('f', 'd', 's', 'a')]),
+         ('asdf', 2,
+            [('a', 's'),
+             ('a', 'd'),
+             ('a', 'f'),
+             ('s', 'a'),
+             ('s', 'd'),
+             ('s', 'f'),
+             ('d', 'a'),
+             ('d', 's'),
+             ('d', 'f'),
+             ('f', 'a'),
+             ('f', 's'),
+             ('f', 'd')])
+            )
+        
+    def testPointValueKnownValues(self):
+        """point_value should give known result with known input"""
+        for word, points in self.pointKnownValues:
+            result = wwf.point_value(word)
+            self.assertEqual(points, result)
+    
+    def testPermutationsKnownValues(self):
+        """permutations should give known result with known input"""
+        for word, r, perms in self.permutationsKnownValues:
+            result = list(wwf.permutations(word, r))
+            self.assertEqual(perms, result)
+
+class PermutationsBadInput(unittest.TestCase):
+    def testTooFewLetters(self):
+        """permutations should fail with 1 letter input"""
+        self.assertRaises(ValueError, wwf.permutations, "a")
+    
+    def testRTooSmall(self):
+        """permutations should fail when r < 2 """
+        self.assertRaises(ValueError, wwf.permutations, ("abc", 1))
+        
+
+if __name__ == '__main__':
+    unittest.main()
+                    
